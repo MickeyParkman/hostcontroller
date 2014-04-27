8586a2509aec1c5112b1f42ffe63a9e7a2c2819a
soargsm <gsmoore@2UA314038M.cos.agilent.com>
2014-04-27 01:22:55 -0700

    Merge origin/master
    Conflicts:
    	src/Communications/FilterListener.java

--- src/Communications/FilterListener.java
+++ src/Communications/FilterListener.java
@@ -10,7 +10,62 @@ import java.util.ArrayList;
 public class FilterListener extends MessageListener
 {
 
-    int active_drum; //  This needs to be persistent, where to put?
+    private final float timeTick = 1.0f / 64.0f;
+    private final int numbDrums = 1;
+    private final int numbMotors = 1;
+    private int referenceMotor = 0;
+
+    //  variables for CIC3 filters, 
+    private final int decimationFactor = 8;
+    private final float gainCIC = decimationFactor ^ 3;
+    private final float groupDelay = ((decimationFactor - 1) / 2.0f)
+            * 3.0f * timeTick;
+
+    private int downCounter;
+
+    private final int numbCICs = 4;
+    //   0 -  tension, 1 - cable angle, 2 - cable out, 3 - motor speed
+    private int[] i1 = new int[numbCICs];
+    private int[] i2 = new int[numbCICs];
+    private int[] i3 = new int[numbCICs];
+    private int[] c1 = new int[numbCICs];
+    private int[] c2 = new int[numbCICs];
+    private int[] c3 = new int[numbCICs];
+
+    //  variables, scales, and offsets for last values recieved in messages
+    private int[] lastTension = new int[numbDrums];
+    private final float tensionScale = 1.0f / 4.0f;
+    private final int tensionOffset = 1024;
+
+    private int[] lastCableAngle = new int[numbDrums];
+    private final float cableAngleScale = 1.0f / 2.0f;  //  degrees per lsb    
+    private final int cableAngleOffset = 40;
+
+    private int[] lastCableOut = new int[numbDrums];
+    private final float cableOutScale = 1.0f / 16.0f;
+    private final int cableOutOffset = 4096;
+
+    private int[] lastMotorSpeed = new int[numbMotors];
+    private final float motorSpeedScale = 1.0f / 128.0f;
+
+    private double startTime;
+
+    private int lastIntTime;
+    private int lastFracTime;
+    private double lastTime;
+    private double currentTime;
+
+    private int lastControlLever;
+    private final float controlLeverScale = 1.0f / (2.0f * 100);  // 1/2 percent per bit
+    private final int controlLeverOffset = 28;
+
+    private int lastState;
+    private int activeDrum;
+    private boolean launchActive;
+
+    
+
+    InternalMessage intData = new InternalMessage();
 
     ArrayList<MessageListener> listeners = new ArrayList();
     MessagePipeline pipeline;
@@ -29,74 +84,168 @@ public class FilterListener extends MessageListener
         int result = msgin.convert_msgtobin(message);
         System.out.println(msgin.id & 0xffe00000);
 
+        int motor = msgin.id & 0x00600000;
+        int drum  = msgin.id & 0x00e00000;
+
         switch (msgin.id & 0xffe00000)
         {
             case 0x20000000:
                 //  Time message
                 System.out.println("Time Message");
-                
-               pipeline.loggMessage(msgin.get_int(0), message);
-                
-                
-                
-                
-                InternalMessage lnchdata = new InternalMessage(data);
-                pipeline.sendinternalmsg(lnchdata);
-                
-                
-                
-                
-                break;
 
-            case 0x21000000:
-                //  Brake Command
-                System.out.println("Brake Command");
-                pipeline.loggMesssage(message);
-                
-                
-                break;
+                if (msgin.dlc == 1)
+                {
+                    //  fractional second tick
+                    lastFracTime = msgin.get_byte(0);
+                    currentTime = (lastIntTime) + lastFracTime
+                            * (1.0f / 64);
+                    //  ??  should there be gg in logg
+                    pipeline. loggMessage(message);
 
-            case 0x22000000:
-                //  Guillotine command
-                System.out.println("Guillotine Command");
-                break;
+                } else if (msgin.dlc == 4)
+                {
+                    //  intergral second
+                    lastIntTime = msgin.get_int(0);
+                    lastFracTime = 0;
+                    
+                    pipeline.loggMessage(currentTime = lastIntTime
+                            , message);
+                } else
+                {
+                    System.out.println("Invalid Time Message Payload Length");
+                }
 
-            case 0x23000000: //  Contactor command
+                //   Update CIC filter integrators
+                //   0 -  tension, 1 - cable angle, 2 - cable out, 3 - motor speed
+                i1[0] += lastTension[activeDrum];
+                i2[0] += i1[0];
+                i3[0] += i2[0];
 
-                System.out.println("Contactor Command");
+                i1[1] += lastCableAngle[activeDrum];
+                i2[1] += i1[1];
+                i3[1] += i2[1];
+
+                i1[2] += lastCableOut[activeDrum];
+                i2[2] += i1[2];
+                i3[2] += i2[2];
+
+                i1[3] += lastMotorSpeed[referenceMotor];
+                i2[3] += i1[3];
+                i3[3] += i2[3];
+
+                downCounter -= 1;
+
+                if (downCounter <= 0)
+                {
+                    downCounter = decimationFactor - 1; //  reset down counter
+                    //  update the comb functions and set the output values
+                    int ctmpe, ctmpo;
+
+                    ctmpo = i3[0] - c1[0];
+                    c1[0] = i3[0];
+                    ctmpe = ctmpo - c2[0];
+                    c2[0] = ctmpo;
+                    ctmpo = ctmpe - c3[0];
+                    c3[0] = ctmpe;
+                    intData.setTension((float) (((double) ctmpo / gainCIC - tensionOffset)
+                            * tensionScale));
+
+                    ctmpo = i3[1] - c1[1];
+                    c1[1] = i3[1];
+                    ctmpe = ctmpo - c2[1];
+                    c2[1] = ctmpo;
+                    ctmpo = ctmpe - c3[1];
+                    c3[1] = ctmpe;
+                    intData.setCableAngle((ctmpo / gainCIC - cableAngleOffset)
+                            * cableAngleScale);
+
+                    ctmpo = i3[2] - c1[2];
+                    c1[2] = i3[2];
+                    ctmpe = ctmpo - c2[2];
+                    c2[2] = ctmpo;
+                    ctmpo = ctmpe - c3[2];
+                    c3[2] = ctmpe;
+                    intData.setCableOut((float) (((double) ctmpo / gainCIC - cableOutOffset)
+                            * cableOutScale));
+
+                    ctmpo = i3[3] - c1[3];
+                    c1[3] = i3[3];
+                    ctmpe = ctmpo - c2[3];
+                    c2[3] = ctmpo;
+                    ctmpo = ctmpe - c3[3];
+                    c3[3] = ctmpe;
+                    intData.setCableSpeed(ctmpo * (motorSpeedScale / gainCIC));
+
+                    //  elasped time includes correction for CIC group delay and
+                    //  data being from one tick before
+                    intData.setElaspedTime(currentTime - startTime
+                            - (groupDelay + timeTick));
+
+                    //  case??
+                    if (launchActive)
+                    {
+                        pipeline.signalnewlaunchdataavaialbe();
+                    }
+
+                }
+
+                lastTime = currentTime;
+
                 break;
 
             case 0x26000000:
                 //  State message
                 System.out.println("State Messge");
-                
-                
-                
-                
-                break;
+                int tmp = msgin.get_ubyte(0);
+                int tmpState = tmp & 0xf;
+                int tmpActiveDrum = tmp & 0xf0;
 
-            case 0x27000000:
-                //  Launch Parameter Request message
-                System.out.println("Launch Parameter Request");
-                
-                pipeline.launchParameterRequest();
-                
-                break;
+                /*
+                 State Assignments
+                 0      SAFE
+                 1      PREP
+                 2      ARMED
+                 3      PROFILE
+                 4      RAMP
+                 5      CONSTANT
+                 6      RECOVERY
+                 7      RETRIEVE
+                 14 (E) STOP
+                 15 (F) ABORT                    
+                 */
+                if (lastState != tmpState)
+                //  state change
+                {
+                    intData.setState(tmpState);
 
-            case 0x50200000:
-                //  control lever statu/data message
+                    if (activeDrum != tmpActiveDrum)
+                    {
+                        intData.setActiveDrum(activeDrum = tmpActiveDrum);
+                    }
 
-                System.out.println("control lever statu/data message");
-                break;
+                    switch (intData.state)
+                    {
+                        case 3:
+                            if (!launchActive)
+                            {
+                                //     new launch begining
+                                launchActive = true;
+                                intData.setStartTime(lastTime);
+                                intData.setElaspedTime(-(groupDelay + timeTick));
+                                pipeline.signalnewlaunchdastarting();   //  ??case
+                                pipeline.signalnewlaunchdataavaialbe(); //  ??case
+                            } else
+                            {
+                                System.out.println("Entry into Profile State with Launch Active");
+                            }
+                            break;
 
-            case 0x51400000:
-                //  zero odometer from embedded controller command
-                System.out.println("zero odometer from embedded controller command");
-                break;
-
-            case 0x51800000:
-                //  zero tensiometer from embedded controller command
-                System.out.println("zero tensiometer from embedded controller command");
+                        case 1:
+                            //  this should be end of launch for demo only
+                            intData.setLauchActive(launchActive = false);
+                            break;
+                    }
+                }
                 break;
 
             case 0xa0200000:
@@ -109,13 +258,55 @@ public class FilterListener extends MessageListener
                 System.out.println("wind message");
                 break;
 
+            case 0x27000000:
+                //  Launch Parameter Request message
+                System.out.println("Launch Parameter Request");
+
+                pipeline.launchParameterRequest();
+                break;
+
+            case 0x50200000:
+                //  control lever statu/data message
+                System.out.println("control lever statu/data message");
+
+                intData.setControlLever(lastControlLever = msgin.get_ubyte(1));
+                break;
+
+            case 0x21000000:
+                //  Brake Command
+                System.out.println("Brake Command");
+                break;
+
+            case 0x22000000:
+                //  Guillotine command
+                System.out.println("Guillotine Command");
+                break;
+
+            case 0x23000000:
+                //  Contactor command
+                System.out.println("Contactor Command");
+                break;
+
+            case 0x51400000:
+                //  zero odometer from embedded controller command
+                System.out.println("zero odometer from embedded controller command");
+                break;
+
+            case 0x51800000:
+                //  zero tensiometer from embedded controller command
+                System.out.println("zero tensiometer from embedded controller command");
+                break;
+
             default:
                 switch (msgin.id & 0xff800000)
                 {
                     case 0x24800000:
                         //  Motor/Controller Status
                         System.out.println("Motor/Controller Status");
+
+                        lastMotorSpeed[motor] = msgin.get_short(1);
                         break;
+
                     case 0x25800000:
                         //  Torque/Speed Command
                         System.out.println("Torque/Speed Command");
@@ -126,17 +317,32 @@ public class FilterListener extends MessageListener
                         {
                             case 0x36000000:
                                 //  active drum status/data during launch message
-                                if (((msgin.id & 0x00e00000) >> 17) != active_drum)
+                                lastCableOut[drum] = msgin.get_short(1);
+
+                                if (drum != activeDrum)
                                 {
                                     //  Mismatched drum indication
                                     System.out.println("Mismatched Drum Indication");
-
                                 }
                                 break;
 
+                            case 0x3800000:
+                                //  active tensiometer status/data during launch
+                                lastTension[drum] = msgin.get_short(1);
+
+                                if (drum != activeDrum)
+                                {
+                                    //  Mismatched tensiometer indication
+                                    System.out.println("Mismatched Drum Indication");
+                                }
+                                break;
+                            
+                            
                             case 0x3a000000:
                                 //  active cable angle status/data during launch message
-                                if (((msgin.id & 0x00e00000) >> 17) != active_drum)
+
+                                lastCableAngle[drum] = msgin.get_ubyte(1);
+                                if (drum != activeDrum)
                                 {
                                     //  Mismatched cable angle sensor indication
                                     System.out.println("Mismatched cable angle sensor indication");
@@ -147,20 +353,16 @@ public class FilterListener extends MessageListener
                                 //  unrecognized id
                                 System.out.println("Unrecognized ID");
                                 System.out.println(msgin.id & 0xffe00000);
+                                break;
 
+                                //  still need decodes for low high/low priority messages
                         }
-
                 }
-
         }
-
     }
 }
 
     // notify all attached listeners after message is filtered
         /*for(MessageListener ml: listeners){
-     ml.msgAvailable(msg);
-     }*/
-    
-
-
+ ml.msgAvailable(msg);
+ }*/
--- src/Communications/InternalMessage.java
+++ src/Communications/InternalMessage.java
@@ -10,111 +10,180 @@ import java.util.ArrayList;
  *
  * @author Matt
  */
-public class InternalMessage {
-    
-    private byte[] canMsg;
-    private int id;
-    private float timestamp;
-    private float cableOut;
-    private float cableSpeed;
-    private float cableAngle;
-    private float tension;
-    private int state;
-    
+public class InternalMessage
+{
+
+    //  CanMsg data
+    //  ??should these be public or private
+    public byte[] canMsg;
+    public double timestamp;    //  Unix time
+    public int state;
+
+    //  launch related data variables
+    private double startTime;       //  start time of launch
+    private float elapsedTime;      //  time since last launch began
+    private float cableOut;         //  cable out (m)
+    private float cableSpeed;       //  cable speed (m/s)
+    private float cableAngle;       //  cable angle (degrees)
+    private float tension;          //  cable tension (N)
+    private float controlLever;     //  control lever nom 0.0 - 1.0
+    private int activeDrum;         //  active drum system
+    private boolean launchActive;   //  true when launch is ongoing
+
     /**
      * Constructs a new InternalMessage from the original CAN message.
      * <p>
      * This constructor is sufficient to create a message that will be written
-     * in the "Log Everything" database.  To add other values to an InternalMessage,
-     * use their corresponding "setter" functions.
-     * 
+     * in the "Log Everything" database. To add other values to an
+     * InternalMessage, use their corresponding "setter" functions.
+     *
      * @param id int value representing the ID of the message
      * @param payload array of bytes containing the message's value
      */
-    public InternalMessage(int id, byte[] payload)
+    public InternalMessage()
     {
-        this.id = id;
-        this.canMsg = payload;
+
     }
-    
-    public InternalMessage(int id, byte[] rawMessage, float timestamp, float cableOut, float cableSpeed, float cabelAngle, float tension, int state) {
-        this.id = id;
-        this.canMsg = rawMessage;
-        this.timestamp = timestamp;
-        this.cableOut = cableOut;
-        this.cableSpeed = cableSpeed;
-        this.cableAngle = cabelAngle;
-        this.tension = tension;
-        this.state = state;
-    }
-    
-    public InternalMessage(float timestamp, float cableOut, float cableSpeed, float cabelAngle, float tension, int state) {
-        this.timestamp = timestamp;
-        this.cableOut = cableOut;
-        this.cableSpeed = cableSpeed;
-        this.cableAngle = cabelAngle;
-        this.tension = tension;
-        this.state = state;
-    }
-    
-    public void setRawMessage(byte[] canMessage) {
+    /*
+     public InternalMessage(byte[] rawMessage, float timestamp, float cableOut, float cableSpeed, float cabelAngle, float tension, int state)
+     {
+     this.id = id;
+     this.canMsg = rawMessage;
+     this.timestamp = timestamp;
+     this.cableOut = cableOut;
+     this.cableSpeed = cableSpeed;
+     this.cableAngle = cabelAngle;
+     this.tension = tension;
+     this.state = state;
+     }
+
+     public InternalMessage(float timestamp, float cableOut, float cableSpeed, float cabelAngle, float tension, int state)
+     {
+     this.timestamp = timestamp;
+     this.cableOut = cableOut;
+     this.cableSpeed = cableSpeed;
+     this.cableAngle = cabelAngle;
+     this.tension = tension;
+     this.state = state;
+     }
+     */
+
+    //  ?? suggest logRawMessage
+    public void setRawMessage(byte[] canMessage)
+    {
         this.canMsg = canMessage;
     }
-    
+
     /**
      * Sets the value of the "timestamp" data member
-     * 
+     *
      * @param timestamp the timestamp of the message, in UNIX time
      */
     public void setTimestamp(float timestamp)
     {
         this.timestamp = timestamp;
     }
-    
-    public void setCableOut(float cableOut) {
+
+    public void setCableOut(float cableOut)
+    {
         this.cableOut = cableOut;
     }
-    
-    public void setCableSpeed(float cableSpeed) {
+
+    public void setCableSpeed(float cableSpeed)
+    {
         this.cableSpeed = cableSpeed;
     }
-    
-    public void setCableAngle(float cableAngle) {
+
+    public void setCableAngle(float cableAngle)
+    {
         this.cableAngle = cableAngle;
     }
-    
-    public void setTension(float tension) {
+
+    public void setTension(float tension)
+    {
         this.tension = tension;
     }
-    
-    public void setState(int state) {
+
+    public void setState(int state)
+    {
         this.state = state;
     }
+
+    public void setStartTime(double time)
+    {
+        this.startTime = time;
+    }
     
+    public void setElaspedTime(double time)
+    {
+        this.elapsedTime = time;
+    }
+    public void setActiveDrum(int drum)
+    {
+        this.activeDrum = drum;
+    }
+    
+    public void setLauchActive(boolean active)
+    {
+        this.launchActive = active;
+    }
+    public void setControlLever(float position)
+    {
+        this.controlLever = position;
+    }
+    
+
     /**
      * Method for acquiring the value of the message's timestamp.
-     * 
+     *
      * @return the timestamp value, if set, null if otherwise.
      */
-    public float getTimestamp()
+    public double getTimestamp()
     {
         return timestamp;
     }
-    
-    public float getCableOut() {
+
+    public float getCableOut()
+    {
         return cableOut;
     }
 
-    public float getCableSpeed() {
+    public float getCableSpeed()
+    {
         return cableSpeed;
     }
-    
-    public float getCableAngle() {
+
+    public float getCableAngle()
+    {
         return cableAngle;
     }
-    
-    public float getTension() {
+
+    public float getTension()
+    {
         return tension;
     }
+    public double getStartTime()
+    {
+        return startTime;
+    }
     
+    public float getElaspedTime()
+    {
+        return elapsedTime;
+    }
+    public int getActiveDrum()
+    {
+        return activeDrum;
+    }
+    
+    public boolean getLaunchActive()
+    {
+        return launchActive;
+    }
+    
+    public float getControlLever()
+    {
+        return controlLever;
+    }
+
 }
