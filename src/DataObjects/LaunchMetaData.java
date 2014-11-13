e67bef8454213fbeb0a3e68b2e96a0e1a9e11fe6
Noah Fujioka <nfujioka@zagmail.gonzaga.edu>
2014-11-12 20:05:07 -0800

    Added LaunchMetaData object
    Class holds the snapshot of all information at the start of a launch, as well as the start and end timestamps in unix time.

---
+++ src/DataObjects/LaunchMetaData.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataObjects;
+
+/**
+ * This class stores information about a launch
+ *
+ * @author Noah Fujioka
+ */
+public class LaunchMetaData {
+    //Profile OperatorProfile
+    Pilot currentPilot;
+    Sailplane currentSailplane;
+    Airfield currentAirfield;
+    Runway currentRunway;
+    Position currentPosition;
+    Parachute currentParachute;
+    DrumParameters currentDrumParameters;
+    Environmental currentEnvironmental; //At the start of the flight
+    long startTimestamp;
+    long endTimestamp;
+    
+    public LaunchMetaData(){
+    }
+    
+    public LaunchMetaData(Pilot newPilot, Sailplane newSailplane,
+            Airfield newAirfield, Runway newRunway, Position newPosition,
+            Parachute newParachute, DrumParameters newDrumParameters,
+            Environmental newEnvironmental, long newStartTimestamp,
+            long newEndTimestamp){
+        //Profile OperatorProfile;
+        currentPilot = newPilot;
+        currentSailplane = newSailplane;
+        currentAirfield = newAirfield;
+        currentRunway = newRunway;
+        currentPosition = newPosition;
+        currentParachute = newParachute;
+        currentDrumParameters = newDrumParameters;
+        currentEnvironmental = newEnvironmental;
+        startTimestamp = newStartTimestamp;
+        endTimestamp = newEndTimestamp;
+    }
+    
+    @Override
+    public String toString() { 
+        return (currentPilot.toString() + ", " + currentSailplane.toString() +
+                ", " + startTimestamp);
+    }
+    
+    public Pilot getPilot(){
+        return currentPilot;
+    }
+    
+    public Sailplane getSailplane(){
+        return currentSailplane;
+    }
+    
+    public Airfield getAirfield(){
+        return currentAirfield;
+    }
+    
+    public Runway getRunway(){
+        return currentRunway;
+    }
+    
+    public Position getPosition(){
+        return currentPosition;
+    }
+    
+    public Parachute getParachtue(){
+        return currentParachute;
+    }
+    
+    public DrumParameters getDrumParameters(){
+        return currentDrumParameters;
+    }
+    
+    public Environmental getEnvironmental(){
+        return currentEnvironmental;
+    }
+    
+    public long getStartTimestamp(){
+        return startTimestamp;
+    }
+    
+    public long getEndTimestamp(){
+        return endTimestamp;
+    }
+}
