7fdf847d29e886e079ed2e726c846776eb05f89b
yacklebeam <jaketroxel@gmail.com>
2015-03-31 14:46:57 -0700

    Added framework for MessageListener

---
+++ src/Communications/DataRelay.java
@@ -0,0 +1,13 @@
+package Communications;
+
+public class DataRelay
+{
+	public DataRelay()
+	{}
+
+	public void sendTimeMessage(){}
+
+	public void sendStateMessage(){}
+
+	public void sendUnknownMessage(){}
+}
