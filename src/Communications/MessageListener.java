172c5fb8b2e639e815128d3e207250452b90173b
soargsm <gsmbamoore@comcast.net>
2015-04-07 20:28:09 -0700

    MessageListern Commit to Temp

--- src/Communications/MessageListener.java
+++ src/Communications/MessageListener.java
@@ -2,30 +2,100 @@ package Communications;
 
 public class MessageListener implements Observer
 {
-    static final int ID_OFFSET = 1 << 21;
-    static final int TIME_MESSAGE_ID = 256 * ID_OFFSET; // 0x200
-    static final int MOTOR_MESSAGE_ID = 296 * ID_OFFSET; // 0x250
-    static final int TORQUE_MESSAGE_ID = 300 * ID_OFFSET; // 0x258
-    static final int STATE_MESSAGE_ID = 304 * ID_OFFSET; // 0x260
-    static final int LAUNCH_PARAM_MESSAGE_ID = 327 * ID_OFFSET; // 0x28e
-    static final int TENSION_MESSAGE_ID = 448 * ID_OFFSET; // 0x380
-    static final int CABLE_ANGLE_MESSAGE_ID = 464 * ID_OFFSET; // 0x3a0
-    static final int PARAM_REQUEST_MESSAGE_ID = 312 * ID_OFFSET; // 0x270
-    static final int CP_CONTROL_LEVER_RMT = 328 * ID_OFFSET; // 0x290
-    static final int CP_CONTROL_LEVER_LCL = 329 * ID_OFFSET; // 0x292
-    static final int CP_CONTROL_LEVER_INPUTS_RMT = 330 * ID_OFFSET; // 0x294
-    static final int CP_CONTROL_LEVER_INPUTS_LCL = 331 * ID_OFFSET; // 0x296
-    static final int CONTROL_LEVER_MESSAGE_ID = 641 * ID_OFFSET; // 0x502   
+    static final float TWO_PI = 2.0f * (float) Math.PI;
+
     //for now, leave these defined here. I am working on a constant loader that will let you define
     // these in a file then have them globally accessible
+    static final int ID_OFFSET = 21;
+    static final int ID_SCALE = 1 << ID_OFFSET;
+    static final int TIME_MESSAGE_ID = 256 * ID_SCALE;              // 0x200
+    static final int MOTOR_MESSAGE_ID = 296 * ID_SCALE;             // 0x250
+    static final int TORQUE_COMMAND_MESSAGE_ID = 300 * ID_SCALE;    // 0x258
+    static final int STATE_MESSAGE_ID = 304 * ID_SCALE;             // 0x260
+    static final int PARAM_REQUEST_MESSAGE_ID = 312 * ID_SCALE;     // 0x270 
+    static final int LAUNCH_PARAM_MESSAGE_ID = 327 * ID_SCALE;      // 0x28e
+        static final int CP_CL_RMT_MESSAGE_ID = 328 * ID_SCALE;     // 0x290
+    static final int CP_CL_LCL_MESSAGE_ID = 329 * ID_SCALE;         // 0x292
+    static final int CP_INPUTS_RMT_MESSAGE_ID = 330 * ID_SCALE;     // 0x294
+    static final int CP_INPUTS_LCL_MESSAGE_ID = 331 * ID_SCALE;     // 0x296
+    static final int CP_OUTPUTS_MESSAGE_ID = 336 * ID_SCALE;        // 0x2A0
+    static final int ORIENTATION_ID = 385 * ID_SCALE;               // 0x302
+    static final int DRUM_MESSAGE_ID = 432 * ID_SCALE;              // 0x360
+    static final int TENSION_MESSAGE_ID = 448 * ID_SCALE;           // 0x380
+    static final int CABLE_ANGLE_MESSAGE_ID = 464 * ID_SCALE;       // 0x3a0
+    static final int DENSITY_ALTITUDE_ID = 689 * ID_SCALE;          // 0x562
+    static final int WIND_ID = 690 * ID_SCALE;                      // 0x564
+    static final int BATTERY_SYSTEM_ID = 704 * ID_SCALE;            // 0x580
+
+    private final int NUMBER_DRUMS = 1;
+    private final int NUMBER_CICS = 5;
+    private final int NUMBER_MOTORS = 1;
+    private float[] torqueGain = new float[NUMBER_MOTORS];
+
+    //static final int HEARTBEAT_COUNT_ID = 60;                //%d only
+    static final int ticsPerSec = 16;   //  should be power of 2
+    static final float secPerTic = 1.0f / ticsPerSec;
+
+    private int currentState;
+    //private int lastState;
+
+    //  variables for CIC3 filters, 
+    static final int filterOuputsPerSecond = 4;
     
-    public int currentTimestamp; //i believe we decided this was an int?
-                    //this would be accessible to classes needing current time
+    private final int decimationFactor = ticsPerSec / filterOuputsPerSecond;
+    private final float gainCIC = decimationFactor * decimationFactor
+            * decimationFactor;
+    private final float cicGroupDelaySamples = ((decimationFactor - 1) / 2.0f)
+            * 3.0f;
+    //  Actual computations use sample values at one tic prior
+    private final float groupDelay = (cicGroupDelaySamples - 1) * secPerTic;
+    private int downCounter;
+
+    private long[] i1 = new long[NUMBER_CICS];
+    private long[] i2 = new long[NUMBER_CICS];
+    private long[] i3 = new long[NUMBER_CICS];
+    private long[] c1 = new long[NUMBER_CICS];
+    private long[] c2 = new long[NUMBER_CICS];
+    private long[] c3 = new long[NUMBER_CICS];
+
+    //  variables, scales, and offsets for last measurements recieved in messages
+    private float[] motorTorque = new float[NUMBER_MOTORS];
+    private float lastCommandedDrumTorque = 0;   //  composite drum torque commanded (no losses)
+    private final float cicTorqueScale = 100;
+    private float filteredCommandedDrumTorque;
+
+    private float lastCableTension = 0;
+    private final float cicTensionScale = 100;
+    private float filteredCableTension;
+
+    private float lastCableSpeed = 0;
+    private final float cicCableSpeedScale = 100;
+    private float filteredCableSpeed;
+    //private final float MotorToCableSpeed = 1.0f / 0.359f;  //  get proper scale factor
+
+    private float lastCableAngle = 0;
+    private final float cicCableAngleScale = 100;
+    private float filteredCableAngle;
+
+    private float lastCableOut = 0;
+    private final float cicCableOutScale = 100;
+    private float filteredCableOut;
+
+    private int intUnixTime;
+
+    private int activeDrum = 0;
+
+    public double currentUnixTime;
+
     private DataRelay relay = null;
-    
+
+    private int nullMessageCnt = 0;
+    private int cpOutputCnt = 0;
+
     public MessageListener()
     {
-        currentTimestamp = 0;
+        currentUnixTime = 0.0;
+        torqueGain[0] = 7.0f;
     }
 
     //my code will call this function
@@ -36,36 +106,288 @@ public class MessageListener implements Observer
 
     public void update(String msg)
     {
+        short status;
+
+        //  check for empty string
+        if (msg.equals(""))
+        {
+            // System.out.println("Null message received: " + ++nullMessageCnt);
+            return;
+        }
         //decode the CANBUS message here
         CanCnvt canIn = new CanCnvt();//I believe this is the correct call
         canIn.convert_msgtobin(msg);
 
+        boolean messageProcessed = false;
         switch (canIn.id)
         {
-            /////////////////////////////
-            //
-            //  GEORGE - YOUR CODE HERE
-            //
-            /////////////////////////////
             case TIME_MESSAGE_ID:
-                //do what we need to do with time messages...
-                if(relay != null) relay.sendTimeMessage(/*...whatever we need here…*/);
-                // any time you call a function from relay, add that IF statement
+            {
+                if (canIn.dlc == 1)
+                {
+                    currentUnixTime = intUnixTime
+                            + canIn.get_byte(0) * secPerTic;
+                } else
+                {
+                    intUnixTime = canIn.get_int(0);
+                    currentUnixTime = intUnixTime;
+                    status = canIn.get_ubyte(4);
+                    if (status != 0)
+                    {
+                        //if(relay != null) relay.sendTimeStatus(status, 
+                        //        intUnixTime);
+                    }                        
+                }
+
+                //   0 - torque 1 -  tension, , 2 - cable speed 3 - cable angle, 
+                //   4 - cable out        
+                updateIntegrators(0, lastCommandedDrumTorque * cicTorqueScale);
+                updateIntegrators(1, lastCableTension * cicTensionScale);
+                updateIntegrators(2, lastCableSpeed * cicCableSpeedScale);
+                updateIntegrators(3, lastCableAngle * cicCableAngleScale);
+                updateIntegrators(4, lastCableOut * cicCableOutScale);
+                if (--downCounter < 0)
+                {
+                    downCounter = decimationFactor - 1; //  reset down counter                    
+                    filteredCommandedDrumTorque = updateCombs(0)
+                            * (1.0f / (gainCIC * cicTorqueScale));
+                    filteredCableTension = updateCombs(1)
+                            * (1.0f / (gainCIC * cicTensionScale));
+                    filteredCableSpeed = updateCombs(2)
+                            * (1.0f / (gainCIC * cicCableSpeedScale));
+                    filteredCableAngle = updateCombs(3)
+                            * (1.0f / (gainCIC * cicCableAngleScale));
+                    filteredCableOut = updateCombs(4)
+                            * (1.0f / (gainCIC * cicCableOutScale));
+                    //  send filtered values
+                    /*  need methods for each of these
+                    //  should this be one function call?????
+                     if(relay != null) relay.sendFilteredTension(filteredTension,
+                     groupDelay);
+                     if(relay != null) relay.sendFilteredTorque(filteredTorque,
+                     groupDelay);
+                     if(relay != null) relay.sendFilteredCableSpeed(filteredCableSpeed,
+                     groupDelay);
+                     if(relay != null) relay.sendFilteredCableAngle(filteredCableAngle,
+                     groupDelay);
+                     if(relay != null) relay.sendFilteredCableOut(filteredCableOut,
+                     groupDelay);
+                     */
+                    System.out.println("Filtered Outputs: " + filteredCommandedDrumTorque
+                            + " " + filteredCableTension + " " + filteredCableSpeed
+                            + " " + filteredCableAngle + " " + filteredCableOut);
+                    messageProcessed = true;                    
+                }
+            break;
+            }
+
+            case ORIENTATION_ID:
+                {
+                    float pitch = canIn.get_halffloat(0);
+                    float roll = canIn.get_halffloat(2);
+                    float magnetic = canIn.get_halffloat(4);
+                    status = canIn.get_byte(2);
+                    System.out.println("Orientation: " + pitch
+                        + " " +  roll + " " + magnetic);
+                    //if(relay != null) relay.sendMagneticStatus(status,
+                    //        pitch, roll, magnetic);                   
+                    messageProcessed = true;
+                    break;      
+                }
+            
+            case STATE_MESSAGE_ID:
+            {
+                currentState = canIn.get_byte(0) & 0x1f;
+                activeDrum = (canIn.get_byte(0) >> 5) & 0x7;
+                System.out.println("State and Active Drum: " + currentState
+                        + "  " +  activeDrum);
+                //if(relay != null) relay.sendState(currentState, activeDrum);
+                messageProcessed = true;
                 break;
-            //continue for other messages
-            /*case STATE_MESSAGE_ID:
-                doMyStateMessageFunction(canIn);
-                if(relay != null) relay.sendStateMessage(...some params...);
+            }
+
+            case PARAM_REQUEST_MESSAGE_ID:
+            {
+                //if(relay != null) relay.sendLaunchParameterRequest();
+                System.out.println("Launch Parameter Request Message Received");
+                messageProcessed = true;
                 break;
-            */
+            }
+        }
+
+        if (messageProcessed == false)
+        {   //  IDs with 3 mapped lsbs
+            int messageDrum = (canIn.id & 0x00e00000) >> ID_OFFSET;  
+            switch (canIn.id & 0xff000000)    
+            {
+                case (DRUM_MESSAGE_ID):
+                {
+                    status = canIn.get_byte(6);
+                    if (status >= 0)
+                    {
+                        if (messageDrum == activeDrum) //  3 lsbs match active drum
+                        {
+                            lastCableOut = canIn.get_halffloat(0);
+                            lastCableSpeed = canIn.get_halffloat(2)
+                                    * canIn.get_halffloat(4) * TWO_PI;
+                        }
+                    } else if (status != 0)
+                    {
+                        //if(relay != null) relay.sendDrumStatus(status, messageDrum, 
+                        //  canIn.get_halffloat(0), canIn.get_halffloat(2), 
+                        //  canIn.get_halffloat(4, ));            
+                    }
+                    messageProcessed = true;
+                    break;
+                }
+
+                case (TENSION_MESSAGE_ID):
+                {
+                    status = canIn.get_byte(3);
+                    if (status >= 0)
+                    {
+                        if (messageDrum == activeDrum) //  3 lsbs match active drum
+                        {
+                            lastCableTension = canIn.get_halffloat(0);
+                        }
+                    } else if(status != 0)
+                    {
+                        //if(relay != null) relay.sendTensionStatus(status, 
+                        //        messageDrum, canIn.get_halffloat(0));            
+                    }
+                    messageProcessed = true;
+                    break;
+                }
+
+                case (CABLE_ANGLE_MESSAGE_ID):
+                {
+                    status = canIn.get_byte(3);
+                    if (status >= 0)
+                    {
+                        if (messageDrum == activeDrum) //  3 lsbs match active drum
+                        {
+                            lastCableAngle = canIn.get_halffloat(0);
+                        }
+                    } else if (status !=0)
+                    {
+                        //if(relay != null) relay.CableAngleStatus(status,
+                        //        messageDrum, canIn.get_halffloat(0));
+                    }
+                    messageProcessed = true;
+                    break;
+                }
+            }
+        }
+
+        if (messageProcessed == false)
+        {   //  IDs with 2 mapped lsbs 
+            int messageMotor = (canIn.id & 0x00600000) >> ID_OFFSET;
+            switch (canIn.id & 0xff800000)    
+            {
+                case MOTOR_MESSAGE_ID:
+                {                    
+                    float rps = canIn.get_halffloat(0);
+                    float revolutions = canIn.get_halffloat(2);
+                    float temperature = (float) canIn.get_byte(4);
+                    status = canIn.get_byte(5);
+                    if (status >= 0)
+                    {
+                        //  nothing needed now
+                    } else if (status != 0)
+                    {
+                        //if(relay != null) relay.sendMotorStatus(status,
+                        //        messageMotor, speed, revolutions,
+                        //        temperature);
+                    }
+                    messageProcessed = true;
+                    break;
+                }
+
+                case TORQUE_COMMAND_MESSAGE_ID: //  command message, no status
+                {
+                    motorTorque[messageMotor] = canIn.get_halffloat(0);
+                    lastCommandedDrumTorque = 0;
+                    //System.out.println("Commanded Motor Torque: " + motorTorque[messageMotor]
+                    //        + "  " + messageMotor);
+                    for (int i = 0; i < NUMBER_MOTORS; i++)
+                    {
+                        lastCommandedDrumTorque += motorTorque[i] * torqueGain[i];
+                    }
+                    messageProcessed = true;
+                    break;   
+                }
+                 
+                case BATTERY_SYSTEM_ID:
+                {
+                    float voltage = canIn.get_halffloat(0);
+                    float current = canIn.get_halffloat(2);
+                    float temperature = (float) canIn.get_byte(4);
+                    status = canIn.get_byte(5);
+                    System.out.println("Battery System: " + voltage
+                        + " " +  current + " " + temperature);
+                    //if(relay != null) relay.sendBatteryStatus(status,
+                    //        messageMotor, voltage, current, temperature);                    messageProcessed = true;
+                    messageProcessed = true;
+                    break;                    
+                }           
+            }
+        }
+        
+        if (messageProcessed == false)
+        {   //  lower priority and occurence messages   
+            switch (canIn.id)
+            {
+                case DENSITY_ALTITUDE_ID:
+                {
+                    float pressure = canIn.get_halffloat(0);
+                    float temperature = (float) canIn.get_byte(2);
+                    float hummidity = (float) canIn.get_byte(3);
+                    status = canIn.get_byte(4);
+                    System.out.println("Density Altitude: " + pressure
+                        + " " +  temperature + " " + hummidity);
+                    //if(relay != null) relay.sendDensityAltitudeStatus(status,
+                    //        pressure, temperature, hummidity);                   
+                    messageProcessed = true;
+                    break;      
+                }
+                
+                case WIND_ID:
+                {
+                    float direction = (float) canIn.get_byte(0);
+                    float speed = canIn.get_halffloat(2);                    
+                    float gust = (float) canIn.get_byte(4);
+                    status = canIn.get_byte(6);
+                    System.out.println("Wind: " + direction
+                        + " " +  speed + " " + gust);
+                    //if(relay != null) relay.sendWindStatus(status,
+                    //        speed, direction);                   
+                    messageProcessed = true;
+                    break;      
+                }                
+            }
         }
     }
 
-    /////////////////////////////////
-    //
-    //  GEORGE - YOUR FUNCTIONS HERE
-    //
-    /////////////////////////////////
+    private void updateIntegrators(int filterIndex, float filterInput)
+    {
+        i1[filterIndex] += Math.round(filterInput);
+        i2[filterIndex] += i1[filterIndex];
+        i3[filterIndex] += i2[filterIndex];
+    }
 
-    public void update(){} //this guy is not used, but needed because we are implementing Observer
+    private float updateCombs(int filterIndex)
+    {
+        long ctmpe, ctmpo;
+        ctmpo = i3[filterIndex] - c1[filterIndex];
+        c1[filterIndex] = i3[filterIndex];
+        ctmpe = ctmpo - c2[filterIndex];
+        c2[filterIndex] = ctmpo;
+        ctmpo = ctmpe - c3[filterIndex];
+        c3[filterIndex] = ctmpe;
+        return ((float) ctmpo);
+    }
+
+    public void update()
+    {
+    } //this guy is not used, but needed because we are implementing Observer
 }
